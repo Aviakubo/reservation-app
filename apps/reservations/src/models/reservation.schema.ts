3225d7520512fe7615b2f11a78d981117aee06ca
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2023-10-17 18:45:59 -0400

    creating models and routes

---
+++ apps/reservations/src/models/reservation.schema.ts
@@ -0,0 +1,13 @@
+import { AbstractDocument } from "@app/common/database/abstract.schema";
+import { Schema } from "@nestjs/mongoose";
+
+@Schema({ versionKey: false })
+
+export class ReservationDocument extends AbstractDocument {
+    timestamp: Date;
+    startDate: Date;
+    endDate: Date;
+    userId: string;
+    placeId: string;
+    invoiceId: string;
+}
--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -1,8 +1,27 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ReservationDocument } from "apps/reservations/src/models/reservation.schema";
 import { SchemaTypes, Types } from "mongoose";
 
 @Schema ()
 export class AbstractDocument {
-    @Prop({ type: SchemaTypes.ObjectId})
-    _id: Types.ObjectId;
+    @Prop()
+    timestamp: Date;
+
+    @Prop()
+    startDate: Date;
+
+    @Prop()
+    endDate: Date;
+
+    @Prop()
+    userId: string;
+
+    @Prop()
+    placeId: string;
+
+    @Prop()
+    invoiceId: string;
 }
+
+export const ReservationSchema = 
+    SchemaFactory.createForClass(ReservationDocument);

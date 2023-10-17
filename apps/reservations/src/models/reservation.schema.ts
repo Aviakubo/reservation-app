0dbfd91e82dc6f0ac3e7b82258938d1dc80424a1
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2023-10-17 18:58:45 -0400

    fixed some errors

--- apps/reservations/src/models/reservation.schema.ts
+++ apps/reservations/src/models/reservation.schema.ts
@@ -1,13 +1,26 @@
-import { AbstractDocument } from "@app/common/database/abstract.schema";
-import { Schema } from "@nestjs/mongoose";
+import { AbstractDocument } from '@app/common';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ versionKey: false })
-
 export class ReservationDocument extends AbstractDocument {
-    timestamp: Date;
-    startDate: Date;
-    endDate: Date;
-    userId: string;
-    placeId: string;
-    invoiceId: string;
+  @Prop()
+  timestamp: Date;
+
+  @Prop()
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+
+  @Prop()
+  userId: string;
+
+  @Prop()
+  placeId: string;
+
+  @Prop()
+  invoiceId: string;
 }
+
+export const ReservationSchema =
+  SchemaFactory.createForClass(ReservationDocument);
--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -1,27 +1,8 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { ReservationDocument } from "apps/reservations/src/models/reservation.schema";
+import { Prop, Schema } from "@nestjs/mongoose";
 import { SchemaTypes, Types } from "mongoose";
 
 @Schema ()
 export class AbstractDocument {
-    @Prop()
-    timestamp: Date;
-
-    @Prop()
-    startDate: Date;
-
-    @Prop()
-    endDate: Date;
-
-    @Prop()
-    userId: string;
-
-    @Prop()
-    placeId: string;
-
-    @Prop()
-    invoiceId: string;
+    @Prop({ type:SchemaTypes.ObjectId })
+    _id: Types.ObjectId
 }
-
-export const ReservationSchema = 
-    SchemaFactory.createForClass(ReservationDocument);

35b09b997760ec49bd96c546427599431ab65e16
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2023-10-25 10:59:09 -0400

    started on the users schema and verification microservice

---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository) {}
+    async create(createUserDTO: CreateUserDTO) {
+        return this,this.usersRepository.create(createUserDTO);
+    }
+}

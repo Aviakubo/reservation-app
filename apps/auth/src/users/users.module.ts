56ffcb4d181f9f4e0877f56167cf39e9b9a13576
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2023-10-30 23:31:58 -0400

    fixed an issue with providers in users module

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { DatabaseModule } from '@app/common';
 import { UserDocument, UserSchema } from './models/user.schema';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { UserDocument, UserSchema } from './models/user.schema';
     ])
   ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository]
 })
 export class UsersModule {}

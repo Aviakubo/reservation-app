3177e5102d12b1fb710558c0b742ef993d162d74
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2023-11-02 17:11:24 -0400

    added jwt authentication strategy

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { CreateUserDTO } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -21,5 +22,9 @@ export class UsersService {
             throw new UnauthorizedException('Credentials are not valid.')
         }
         return user;
+    }
+
+    async getUser(getUserDto: GetUserDto) {
+        this. usersRepository.findOne(getUserDto);
     }
 }
